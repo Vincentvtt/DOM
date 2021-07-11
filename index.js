3ef33fe155527abc211b35a01848b70ff6d8b9eb
Vincent Tsai <vincentvtt@hotmail.com>
2021-07-12 01:12:38 +1000

    feat: added jquery testing

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 console.log("Hello from external javascript file");
 document.querySelector("h1").innerText = "Good bye!";
 document.querySelector("a").setAttribute("href","test.com");
+
+$("h1").css("color", "blue");
+$("li").css("fontSize", "3rem");
+$("button").click(function() {
+    $("ul").slideToggle( "slow", function() {
+      // Animation complete.
+    });
+  })
